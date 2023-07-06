e571562d2f9cbe34107d84cd2fe1c5df1be9548e
Jessica Susilo <jxsusilo@gmail.com>
2023-07-06 01:39:50 -0700

    day1 progress

---
+++ src/pages/Title.js
@@ -0,0 +1,17 @@
+function Title() {
+    return (
+        <div>
+            <img id="happybday"></img>
+            <img id="pris"></img>
+            <img id="play"></img>
+        </div>
+        
+    )
+    // return (
+    //     <div className="page">
+    //         <h1>Happy<br/>&ensp;Birthday,<br/>&emsp;&emsp;&emsp;<span id="pris">Pris!</span></h1>
+    //     </div>
+    // )
+}
+
+export default Title;
