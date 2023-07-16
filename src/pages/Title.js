29f6eb9c2821ff43d867363636cfd3e3782bb843
Jessica Susilo <jxsusilo@gmail.com>
2023-07-15 23:55:18 -0700

    update

--- src/pages/Title.js
+++ src/pages/Title.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
 function Title() {
-    return (
-        <div>
-            <img id="happybday"></img>
-            <img id="pris"></img>
-            <img id="play"></img>
-        </div>
-        
-    )
+    const [play, setPlay] = useState(0);
+    
+    if (play === 0) {
+        return (
+            <div>
+                <img id="happybday"></img>
+                <img id="vines"></img>
+                <img id="pris"></img>
+                <img id="play" onClick={(e) => setPlay(1)}></img>
+            </div>
+            
+        )
+    } else if (play === 1) {
+        return (
+            <div>
+                <h1>under construction...</h1>
+                <img id="strawb"></img>
+            </div>
+            
+        )
+    }
+    
     // return (
     //     <div className="page">
     //         <h1>Happy<br/>&ensp;Birthday,<br/>&emsp;&emsp;&emsp;<span id="pris">Pris!</span></h1>
